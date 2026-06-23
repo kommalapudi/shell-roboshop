1ba320a05a01412384fa033b9f1ab9f83ef78764
chaitanya <kommalkrishna@gmail.com>
2026-06-23 16:31:32 +0530

    update code with tag specs

--- roboshop.sh
+++ roboshop.sh
@@ -9,22 +9,22 @@ do
     --image-id $AMI_ID \
     --instance-type t3.micro \
     --security-group-ids $SG_ID \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
-    --query 'Instances[0].InstanceId' \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Reservations[0].Instances[0].InstanceId' \
     --output text )
 
     if [ $instance == "frontend" ]; then
         IP=$( 
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
-            --query 'Reservations[].Instances[0].PublicIpAddress' \
+            --query 'Reservations[0].Instances[0].PublicIpAddress' \
             --output text 
         )
     else
         IP=$( 
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
-            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
             --output text 
         )
     fi
