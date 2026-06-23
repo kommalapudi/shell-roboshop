122a08a1125d04d37501e31f3d4f20f875b89ff1
chaitanya <kommalkrishna@gmail.com>
2026-06-23 15:51:09 +0530

    update code with instance id variable

--- roboshop.sh
+++ roboshop.sh
@@ -5,12 +5,12 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-aws ec2 run-instances \
+aws instance_id = $( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t3.micro \
     --security-group-ids $SG_ID \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
 #    --query 'Reservations[0].Instances[0].PrivateIpAddress' \
     --query 'Reservations[0].Instances[0].InstanceID' \
-    --output text
+    --output text )
 done
