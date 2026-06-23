c69a2f9003556a4ec79b59eba771bd8f64d313e3
chaitanya <kommalkrishna@gmail.com>
2026-06-23 16:00:00 +0530

    update instance id name

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-    instance_id = $( aws ec2 run-instances \
+    INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t3.micro \
     --security-group-ids $SG_ID \
@@ -17,14 +17,14 @@ do
     if [ $instance == "frontend" ]; then
         IP=$( 
             aws ec2 describe-instances \
-            --instance-ids $instance_id \
+            --instance-ids $INSTANCE_ID \
             --query 'Reservations[0].Instances[0].PublicIpAddress' \
             --output text 
         )
     else
         IP=$( 
             aws ec2 describe-instances \
-            --instance-ids $instance_id \
+            --instance-ids $INSTANCE_ID \
             --query 'Reservations[0].Instances[0].PrivateIpAddress' \
             --output text 
         )
