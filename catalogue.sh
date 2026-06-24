c6d1147e9bce4268980b3f227ec93bb38fedfe7f
chaitanya <kommalkrishna@gmail.com>
2026-06-24 22:04:32 +0530

    update catalogue file

---
+++ catalogue.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ "$USERID" -ne 0 ]; then
+    echo -e "$R Please run as root $N" | tee -a "$LOGS_FILE"
+    exit 1
+fi
+
+mkdir -p "$LOGS_FOLDER"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R$2... failed.$N" | tee -a "$LOGS_FILE"
+        exit 1
+    else
+        echo -e "$2...$G succeeded.$Y" | tee -a "$LOGS_FILE"
+    fi
+}
+
+
+dnf module disable nodejs -y &>> "$LOGS_FILE"
+VALIDATE $? "disabling nodejs default version" 
+
+dnf module enable nodejs:20 -y &>> "$LOGS_FILE"
+VALIDATE $? "enabling nodejs module"
+
+dnf install nodejs -y &>> "$LOGS_FILE"
+VALIDATE $? "installing nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "creating roboshop user"
+
+mkdir /app
+VALIDATE $? "creating /app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? "downloading catalogue component"
