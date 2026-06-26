49ff44e08677426df00c93c82c5d4292cd3c488f
chaitanya <kommalkrishna@gmail.com>
2026-06-26 16:11:20 +0530

    update roboshop.sh file

--- roboshop.sh
+++ roboshop.sh
@@ -5,62 +5,58 @@ AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z004782564HOFLM6URBO"
 DOMAIN_NAME="kcdevops.online"
 
-if ! command -v aws >/dev/null 2>&1; then
-    echo "ERROR: AWS CLI is not installed or not in PATH."
-    echo "Install AWS CLI and retry: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html"
-    exit 1
-fi
-
-for instance in "$@"
+for instance in $@
 do
     INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
-    --instance-type t3.micro \
+    --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text )
 
     if [ $instance == "frontend" ]; then
-        IP=$( 
+        IP=$(
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
-            --query 'Reservations[0].Instances[0].PublicIpAddress' \
-            --output text 
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
         )
-        RECORD_NAME="$DOMAIN_NAME" # kcdevops.online
+        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
     else
-        IP=$( 
+        IP=$(
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
-            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
-            --output text 
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
         )
-        RECORD_NAME="$instance.$DOMAIN_NAME" # component.kcdevops.online
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
     fi
 
-    echo "IP Address of $instance is $IP"
+    echo "IP Address: $IP"
 
     aws route53 change-resource-record-sets \
-    --hosted-zone-id "$ZONE_ID" \
-    --change-batch '{
-        "Comment": "Updating DNS record",
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
         "Changes": [
             {
-                "Action": "UPSERT",
-                "ResourceRecordSet": {
-                    "Name": "'$RECORD_NAME'",
-                    "Type": "A",
-                    "TTL": 1,
-                    "ResourceRecords": [
-                        {
-                            "Value": "'$IP'"
-                        }
-                    ]
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
                 }
+                ]
+            }
             }
         ]
-    }'
+    }
+    '
 
     echo "record updated for $instance"
 
